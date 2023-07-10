f4dd60d23ee322e0e3848bd4ad095f36cca8ee76
tomaquet18 <tomaquet18@pop-os.localdomain>
2023-07-10 23:40:24 +0200

    Create App component

---
+++ frontend/src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <header>
+      <router-link to="/">
+        <h1>Awesome Blog</h1>
+      </router-link>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
