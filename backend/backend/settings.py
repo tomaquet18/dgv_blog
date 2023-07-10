519ddeb5f2654036e5702db013b1bea0131c7c00
tomaquet18 <tomaquet18@pop-os.localdomain>
2023-07-10 12:00:25 +0200

    Create GraphSQL schema

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog"
+    "blog",
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+  "SCHEMA": "blog.schema.schema",
+}
