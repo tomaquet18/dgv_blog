08d90131314d2181e6d313811c5fc0db454ce97a
tomaquet18 <tomaquet18@pop-os.localdomain>
2023-07-10 12:06:50 +0200

    Install django-cors-headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "blog",
     "graphene_django",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
