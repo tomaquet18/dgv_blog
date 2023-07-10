0beb96fbb0c533f73553b077bef044d949c670e7
tomaquet18 <tomaquet18@pop-os.localdomain>
2023-07-11 00:06:09 +0200

    Fix ReferenceError: regeneratorRuntime is not defined

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import { createProvider } from './vue-apollo'
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 import router from '@/router'
 
