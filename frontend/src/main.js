9eb9a368dfe07ec797cd06ba5720ca7d1eb8e726
tomaquet18 <tomaquet18@pop-os.localdomain>
2023-07-11 13:44:48 +0200

    Install bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,11 @@ import "regenerator-runtime/runtime";
 
 import router from '@/router'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+
 Vue.config.productionTip = false
 
 new Vue({
